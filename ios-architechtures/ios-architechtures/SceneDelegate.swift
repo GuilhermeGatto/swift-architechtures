fc1ab931892b3853ce0c2302cbe79e0a4787fc14
Guilherme Gatto <guuilhermegatto@hotmail.com>
2024-06-06 20:21:08 -0300

    inital setup

---
+++ ios-architechtures/ios-architechtures/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  ios-architechtures
+//
+//  Created by Guilherme Gatto on 06/06/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        let navigationController = UINavigationController()
+        
+        window = UIWindow(frame: .zero)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = navigationController
+        window?.windowScene = windowScene
+    }
+}
+
